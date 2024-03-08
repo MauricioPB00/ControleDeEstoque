fb97b514389ea45a464c05ddd06d29e892818749
MauricioPB00 <mauricioperinbecker@gmail.com>
2024-03-08 16:43:58 -0300

    Front para receber foto

--- routing-controleEstoque/src/app/cadastro/cadastro.component.ts
+++ routing-controleEstoque/src/app/cadastro/cadastro.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CadastroService } from '../AuthService/cadastro.service';
 
-import { Register } from '../models/register';
+interface FormData {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  password2: string;
+  cpf: string;
+  rg: string;
+  datNasc: string;
+  cidade: string;
+  horTrab: string;
+  wage: string;
+  job: string;
+  horini1: string;
+  horini2: string;
+  horini3: string;
+  horini4: string;
+  teste: string;
+}
 
 @Component({
   selector: 'app-cadastro',
@@ -12,25 +28,79 @@ import { Register } from '../models/register';
 export class CadastroComponent {
 
 
-  constructor(
-    private http: HttpClient,
-    private cadastroService: CadastroService
-  ) { }
-  cadastro: any;
-  dados: Register = { username: "", password: "", email: "", permi: "" };
+  imageUrl: string | ArrayBuffer | null = null;
 
-  ngOnInit(): void {
-    this.dados.username = "";
-    this.dados.password = "";
-    this.dados.email = "";
-    this.dados.permi = "";
-  }
-  cadastrar() {
-    console.log('datacadastar', this.dados.username, this.dados.password, this.dados.email, this.dados.permi);
-    return this.cadastroService.cadastrar(this.dados.username, this.dados.password, this.dados.email, this.dados.permi).subscribe((data: {}) => {
-      this.cadastro = data;
-      console.log(this.cadastro);
-    });
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.imageUrl = reader.result;
+    };
+
+    reader.readAsDataURL(file);
   }
 
+  formData: FormData = {
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    password2: '',
+    cpf: '',
+    rg: '',
+    datNasc: '',
+    cidade: '',
+    horTrab: '',
+    wage: '',
+    job: '',
+    horini1: '',
+    horini2: '',
+    horini3: '',
+    horini4: '',
+    teste: ''
+  };
+
+  currentStep: number = 0;
+
+  nextStep() {
+    this.currentStep++;
+  }
+
+  previousStep() {
+    this.currentStep--;
+  }
+  teste(){
+
+  }
+  finish() {
+    console.log('Form Data:', this.formData);
+    console.log('Arquivo selecionado:', this.imageUrl);
+    const formDataWithFile = { ...this.formData, imageUrl: this.imageUrl };
+    console.log('Form Dataaaaaaaaa:', formDataWithFile);
+  }
+
+  resetForm() {
+    this.currentStep = 0;
+    this.formData = {
+      name: '',
+      username: '', 
+      email: '',    
+      password: '', 
+      password2: '',
+      cpf: '',
+      rg: '',
+      datNasc: '',
+      cidade: '',
+      horTrab: '',
+      wage: '',
+      job: '',
+      horini1: '',
+      horini2: '',
+      horini3: '',
+      horini4: '',
+      teste: ''
+
+    };
+  }
 }
