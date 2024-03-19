080e8d410c9a9259499e406eb644d8c15b1be6c9
MauricioPB00 <mauricioperinbecker@gmail.com>
2024-03-19 16:21:57 -0300

    foto no front

--- routing-controleEstoque/src/app/side-bar/side-bar.component.ts
+++ routing-controleEstoque/src/app/side-bar/side-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router  } from '@angular/router';
+import { SidebarService } from '../AuthService/sidebar.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -11,14 +12,24 @@ export class SideBarComponent implements OnInit, AfterViewInit{
   isSidebarActive: boolean = false;
   admin: boolean = false;
   showApprovalItems: boolean = false;
+  data: any;
   
-  constructor(private router: Router) {}
+  constructor(private router: Router, private sidebarService: SidebarService) {}
 
   ngOnInit(): void {
     const permiValue = JSON.parse(localStorage.getItem('ControleUsuarioPermi') || '{}');
     if (permiValue === '1') { 
       this.admin = true;
     }
+    var userId = JSON.parse(localStorage.getItem('ControleUsuario') || '{}');
+    userId = userId.id;
+    this.sidebarService.getBuscarFoto(userId).subscribe(
+      (data) => {
+        this.data = data;
+      },
+      (error) => {
+      }
+    );
   }
 
   ngAfterViewInit(): void {
