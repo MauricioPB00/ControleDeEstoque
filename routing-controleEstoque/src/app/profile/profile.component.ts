d0fa8f6d94a04990e3355a2755792f40cf7ac62b
MauricioPB00 <mauricioperinbecker@gmail.com>
2024-03-27 01:01:24 -0300

    Profile

--- routing-controleEstoque/src/app/profile/profile.component.ts
+++ routing-controleEstoque/src/app/profile/profile.component.ts
@@ -1,7 +1,26 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../AuthService/profile.service';
 import { TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+export interface Usuario {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  datNasc: string;
+  cpf: string;
+  cidade: string;
+  rg: string;
+  horTrab: string;
+  horaIni: string;
+  horIniFim: string;
+  horFimAft: string;
+  horIniAft: string;
+  wage: string;
+  job: string;
+  file: string;
+}
 
 @Component({
   selector: 'app-profile',
@@ -10,6 +29,7 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class ProfileComponent {
   modalRef: BsModalRef;
+  usuario: Usuario;
 
   constructor(private profileService: ProfileService, private modalService: BsModalService,) { }
   registros: any[] = [];
@@ -17,22 +37,10 @@ export class ProfileComponent {
   editModal: TemplateRef<any>;
 
   ngOnInit(): void {
-    //this.getUsuarioProfile();
     this.getAllUsuarioProfile();
-   
   }
-  // getUsuarioProfile(){
-  //   const userId = JSON.parse(localStorage.getItem('ControleUsuario') || '{}').id
-  //   this.profileService.getUsuarioProfile(userId).subscribe(
-  //     (data) => {
-  //       this.registros = data;
-  //       console.log(this.registros);
-  //     },
-  //     (error) => {
-  //     }
-  //   );
-  // }
-  getAllUsuarioProfile(){
+
+  getAllUsuarioProfile() {
     this.profileService.getAllUsuarioProfile().subscribe(
       (data) => {
         this.registros = data;
@@ -42,13 +50,33 @@ export class ProfileComponent {
       }
     );
   }
+
   openEditModal(template: TemplateRef<any>, userId: number) {
+    this.disableSideBar();
+      this.profileService.getUsuarioProfile(userId).subscribe(
+        (data) => {
+          this.usuario = data;
+          console.log( this.usuario);
+        },
+        (error) => {
+        }
+      );
 
-    console.log("Abrir modal para edição do usuário com ID:", userId);
-     this.modalRef = this.modalService.show(template);
+    this.modalRef = this.modalService.show(template);
   }
-
+  disableSideBar() {
+    const sideBar = document.querySelector('.container');
+    if (sideBar) {
+      sideBar.classList.add('side-bar-disabled');
+    }
+  }
   closeModal() {
     this.modalRef.hide();
+    const sideBar = document.querySelector('.container');
+    if (sideBar) {
+      sideBar.classList.remove('side-bar-disabled');
+    }
   }
+
+ 
 }
