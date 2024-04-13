2e79c8ebec61d6373ab6bfd38bbe621809dc5bf4
MauricioPB00 <mauricioperinbecker@gmail.com>
2024-04-13 15:45:33 -0300

    melhoria na funcao calcularHoraMes

--- routing-controleEstoque/src/app/painel/painel.component.ts
+++ routing-controleEstoque/src/app/painel/painel.component.ts
@@ -5,25 +5,13 @@ import { TemplateRef } from '@angular/core';
 import { PainelService } from '../AuthService/painel.service';
 import { startOfMonth, endOfMonth, eachDayOfInterval, isSaturday, isSunday, parseISO, isWeekend, getDaysInMonth } from 'date-fns';
 
-
-
-interface Registro {
-  id: number;
-  date: string;
-  time: string;
-  user_id: string;
-  horTrab: string;
-  barradeProgresso: string;
-}
-
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
-  response: Registro[] = [];
-  registros: any[] = [];
+  registrosPorUsuarioObjeto: any;
 
   constructor(
     private toastr: ToastrService,
@@ -50,10 +38,18 @@ export class PainelComponent implements OnInit {
       weekend1: any[]
       weekend2: any[]
       weekend3: any[]
-      totalHorasWeekend1: number;
-      totalHorasWeekend2: number;
-      totalHorasWeekend3: number;
-      diasUteis:number
+      totalHorasDiasSemana: number;
+      totalHorasSabado: number;
+      totalHorasDomingo: number;
+      diasTrabalhados: number;
+      diasUteis: number;
+      diasFaltados: number;
+      horasFaltando: number;
+      horasNoMesTrabalhadas: number,
+      mes: number,
+      ano: number,
+      user:string,
+      progressBar:string,
     }>();
 
     data.forEach(registro => {
@@ -64,24 +60,45 @@ export class PainelComponent implements OnInit {
           weekend1: [],
           weekend2: [],
           weekend3: [],
-          totalHorasWeekend1: 0,
-          totalHorasWeekend2: 0,
-          totalHorasWeekend3: 0,
+          totalHorasDiasSemana: 0,
+          totalHorasSabado: 0,
+          totalHorasDomingo: 0,
+          diasTrabalhados: 0,
           diasUteis: 0,
+          diasFaltados: 0,
+          horasFaltando: 0,
+          horasNoMesTrabalhadas: 0,
+          mes:0,
+          ano:0,
+          user:'0',
+          progressBar:'0',
         });
       }
 
       const userRegistros = data.filter(reg => reg.user_id === userId);
-
       registrosPorUsuarioMap.get(userId)!.weekend1 = userRegistros.filter(reg => reg.weekend === '1');
       registrosPorUsuarioMap.get(userId)!.weekend2 = userRegistros.filter(reg => reg.weekend === '2');
       registrosPorUsuarioMap.get(userId)!.weekend3 = userRegistros.filter(reg => reg.weekend === '3');
     });
 
+    /*--------------------------------- 3 -------------------------------- */
+    registrosPorUsuarioMap.forEach((value, userId) => {
+      let totalHorasDomingo = 0;
+      value.weekend3.forEach(registro => {
+
+        const timeParts = registro.time.split(':');
+        const horas = parseInt(timeParts[0]);
+        const minutos = parseInt(timeParts[1]);
+        const segundos = parseInt(timeParts[2]);
+
+        totalHorasDomingo += horas + (minutos / 60) + (segundos / 3600);
+      });
+      value.totalHorasDomingo = totalHorasDomingo;
+    });
 
     /*--------------------------------- 2 -------------------------------- */
     registrosPorUsuarioMap.forEach((value, userId) => {
-      let totalHorasWeekend2 = 0;
+      let totalHorasSabado = 0;
       value.weekend2.forEach(registro => {
 
         const timeParts = registro.time.split(':');
@@ -89,96 +106,71 @@ export class PainelComponent implements OnInit {
         const minutos = parseInt(timeParts[1]);
         const segundos = parseInt(timeParts[2]);
 
-        totalHorasWeekend2 += horas + (minutos / 60) + (segundos / 3600);
+        totalHorasSabado += horas + (minutos / 60) + (segundos / 3600);
       });
-      value.totalHorasWeekend2 = totalHorasWeekend2;
-      // console.log(`Total de horas no weekend2 para o usuário ${userId}: ${totalHorasWeekend2.toFixed(2)}`);
+      value.totalHorasSabado = totalHorasSabado;
     });
-
 
     /*--------------------------------- 1 -------------------------------- */
     registrosPorUsuarioMap.forEach((value, userId) => {
-      let totalHorasWeekend1 = 0;
-      let diasmes
+      let totalHorasDiasSemana = 0;
+      let horasNoMesTrabalhadas = 0;
       value.weekend1.forEach(registro => {
-  
+
         const timeParts = registro.time.split(':');
         const horas = parseInt(timeParts[0]);
         const minutos = parseInt(timeParts[1]);
         const segundos = parseInt(timeParts[2]);
         const diasUteis = this.calcularDiasUteisNoMes(value.weekend1)
-        const totalHorasRegistro = horas + (minutos / 60) + (isNaN(segundos) ? 0 : segundos / 3600);
-        const horTrabParts = registro.horTrab.split(':');
-        const horasTrabalhadas = parseInt(horTrabParts[0]);
-        const minutosTrabalhados = parseInt(horTrabParts[1]);
-        const segundosTrabalhados = parseInt(horTrabParts[2]);
-        const totalHorasTrabalhadas = horasTrabalhadas + (minutosTrabalhados / 60) + (isNaN(segundosTrabalhados) ? 0 : segundosTrabalhados / 3600);
-
-        totalHorasWeekend1 += totalHorasRegistro - totalHorasTrabalhadas;
+        const diasTrabalhados = this.contarItensNoArray(value.weekend1);
+        const totalHorasTrabalhadasNoDia = horas + (minutos / 60) + (isNaN(segundos) ? 0 : segundos / 3600);
+        const HorasParaTrabalhar = this.calcularHoraTrab(value.weekend1)
+        
+        horasNoMesTrabalhadas += totalHorasTrabalhadasNoDia
+        totalHorasDiasSemana += totalHorasTrabalhadasNoDia - HorasParaTrabalhar;
+        value.diasTrabalhados = diasTrabalhados;
         value.diasUteis = diasUteis;
+        value.user = userId
       });
-      value.totalHorasWeekend1 = totalHorasWeekend1;
+      const mes = this.obterMes(value.weekend1[0].date);
+      const ano = this.obterAno(value.weekend1[0].date);
+      value.ano = ano;
+      value.mes = mes;
+      value.horasNoMesTrabalhadas = horasNoMesTrabalhadas;
+      value.totalHorasDiasSemana = totalHorasDiasSemana;
+      value.diasFaltados = value.diasUteis - value.diasTrabalhados
+      const HorasParaTrabalhar = this.calcularHoraTrab(value.weekend1)
+      value.horasFaltando = HorasParaTrabalhar * value.diasFaltados
+      value.progressBar = this.calcularBarraProgresso(HorasParaTrabalhar, value.diasUteis, value.horasNoMesTrabalhadas)
+
     });
 
+    /*---------------------------------   salvar   -------------------------------- */
+    const registrosPorUsuario: { [userId: string]: any } = {};
     registrosPorUsuarioMap.forEach((value, userId) => {
-      console.log(`weekend1 - usuário ${userId} - totalHoras${value.totalHorasWeekend1.toFixed(2)} - dias uteis  ${value.diasUteis}`);
+      registrosPorUsuario[userId] = value;
     });
-
-
-
-
-
-    /*---------------------------------   console   -------------------------------- */
-    const registrosPorUsuarioObjeto: { [userId: string]: any } = {};
-    registrosPorUsuarioMap.forEach((value, userId) => {
-      registrosPorUsuarioObjeto[userId] = value;
-    });
-    console.log('bbbbb', registrosPorUsuarioObjeto);
-
-    //   registrosPorUsuarioMap.forEach((value, userId) => {
-    //     console.log(value)
-    //     console.log(`aaaaaaaaTotal de horas no weekend2 para o usuário ${userId}: ${value.totalHorasWeekend2.toFixed(2)}`);
-    //     console.log(`aaaaaaaaTotal de horas no weekend3 para o usuário ${userId}: ${value.totalHorasWeekend3.toFixed(2)}`);
-    // });
-
+    this.salvarHoraMesTrabalhada(registrosPorUsuario)
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-  getProgressBar(hora: string, horTrab: string, diasUteis: any) {
-    const cargaHorariaDiaria = parseInt(horTrab.split(':')[0]) * 60 + parseInt(horTrab.split(':')[1]);
-    const cargaHorariaMensal = cargaHorariaDiaria * diasUteis;
-    const [horas, minutos, segundos] = hora.split(':').map(Number);
-    const totalMinutos = horas * 60 + minutos;
-    let percentual = (totalMinutos / cargaHorariaMensal) * 100;
-
-    if (percentual >= 0) {
-      percentual = 100;
-    }
-
-    if (percentual < 0) {
-      percentual = 100 + (percentual / 100) * 100;
-    }
+  
+  calcularBarraProgresso(HorasParaTrabalhar: number, diasUteis: number, horasNoMesTrabalhadas: number) {
+    const cargaHorariaMensal = HorasParaTrabalhar * diasUteis;
+    let percentual = (horasNoMesTrabalhadas / cargaHorariaMensal) * 100;
     const progressBar = `${percentual.toFixed(2)}%`;
-
-    console.log(`Hora: ${hora}, Horário de trabalho diário: ${horTrab}, Carga horária mensal: ${cargaHorariaMensal} minutos, Percentual da barra de progresso: ${progressBar}`);
-
     return progressBar;
   }
 
   parsePercentageToInt(percentage: string): number {
-    //html usa
     return parseInt(percentage.replace('%', ''));
+  }
+
+  calcularHoraTrab(value: any[]) {
+    const horTrabParts = value[0].horTrab.split(':');
+    const horasTrabalhadas = parseInt(horTrabParts[0]);
+    const minutosTrabalhados = parseInt(horTrabParts[1]);
+    const segundosTrabalhados = parseInt(horTrabParts[2]);
+    const HorasParaTrabalhar = horasTrabalhadas + (minutosTrabalhados / 60) + (isNaN(segundosTrabalhados) ? 0 : segundosTrabalhados / 3600);
+    return HorasParaTrabalhar
   }
 
   calcularDiasUteisNoMes(registros: any[]) {
@@ -202,13 +194,18 @@ export class PainelComponent implements OnInit {
     const [ano, mes, dia] = data.split('-');
     return parseInt(mes);
   }
+
   obterAno(data: string): number {
     const [ano, mes, dia] = data.split('-');
     return parseInt(ano);
   }
 
-  salvarHoraMesTrabalhada(hora: any, userId: any, mes: any, ano: any, falta: any) {
-    this.painelService.salvarHoraMesTrabalhado(hora, userId, mes, ano, falta).subscribe(
+  contarItensNoArray(array: any[]) {
+    return array.length;
+  }
+
+  salvarHoraMesTrabalhada(registrosPorUsuario:any){ 
+        this.painelService.salvarHoraMesTrabalhado(registrosPorUsuario).subscribe(
       (data) => {
         this.toastr.success('Sucesso ! Horas calculadas');
       },
